58ecf1e95cc23443cf39d7126efbc493a565a1a1
brunagi <brunnabrs97@gmail.com>
2024-02-24 09:34:48 -0300

    componentizaçao e criaçao da header

---
+++ projeto-compon/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header/Header.jsx"
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <h3>Obrigada pelas aulas meninas, vocês são incriveis!</h3>
+    </>
+  )
+}
+
+export default App
---
+++ projeto-compon/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <h1>Bom dia Kaká, bom dia Cacá! s2</h1>
+        </header>
+    )
+}
+
+export default Header
